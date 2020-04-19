1aad0b8d20c56c02cda2a5da4f4885ae42757777
Sid Gowda <sidgowda.9@gmail.com>
2020-04-19 13:20:15 -0400

    added schedulers

--- app/src/main/java/com/android/rxviewmodelutility/sample/SampleViewModel.kt
+++ app/src/main/java/com/android/rxviewmodelutility/sample/SampleViewModel.kt
@@ -3,9 +3,10 @@ package com.android.rxviewmodelutility.sample
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.android.rxviewmodelutility.BaseRxViewModel
 import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class SampleViewModel @Inject constructor(
@@ -19,6 +20,8 @@ class SampleViewModel @Inject constructor(
     fun getName() {
         addDisposable(
             Single.just(sampleInterface.provideName())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({
                     _nameLiveData.value = it
                 }, {
